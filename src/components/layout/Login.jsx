645de7fab8c79986b7f07dac35640fd12c30e8e4
theRealPedroGabriel <pedrogabrieldcastro@gmail.com>
2023-09-12 23:50:30 -0300

    loginaxiosgetnovo

--- src/components/layout/Login.jsx
+++ src/components/layout/Login.jsx
@@ -14,24 +14,26 @@ function Login() {
     event.preventDefault();
     console.log({ email, senha });
 
-    axios.get(`http://localhost:5000/usuarios?email=${email}`).then((rest) => {
-      const usuario = rest.data;
+    axios
+      .get(`https://spotifycloneback.onrender.com/usuarios?email=${email}`)
+      .then((rest) => {
+        const usuario = rest.data;
 
-      if (!usuario) {
-        setError("Usuário Inválido");
-        return;
-      }
+        if (!usuario) {
+          setError("Usuário Inválido");
+          return;
+        }
 
-      console.log(usuario);
+        console.log(usuario);
 
-      if (usuario.senha != senha) {
-        setError("Dados Inválidos");
-        return;
-      }
+        if (usuario.senha != senha) {
+          setError("Dados Inválidos");
+          return;
+        }
 
-      localStorage.setItem("usuarioLogado", JSON.stringify(usuario));
-      history.push("/lista");
-    });
+        localStorage.setItem("usuarioLogado", JSON.stringify(usuario));
+        history.push("/lista");
+      });
 
     setTimeout(() => {
       history("/lista");
@@ -40,64 +42,65 @@ function Login() {
 
   return (
     <section>
-    <div className="container">
-      <div className="row justify-content-center">
-        <div className="col-md-6">
-          <form onSubmit={handleSubmit}>
-            <h1>Informe seus dados e comece a curtir.</h1>
+      <div className="container">
+        <div className="row justify-content-center">
+          <div className="col-md-6">
+            <form onSubmit={handleSubmit}>
+              <h1>Informe seus dados e comece a curtir.</h1>
 
-            <div className="form-group mb-3">
-              <label htmlFor="email" className="email-cadastro">
-                Qual o seu e-mail?
-              </label>
-              <input
-                type="email"
-                className="form-control"
-                id="email"
-                placeholder="Insira seu e-mail."
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                required
-              />
-            </div>
+              <div className="form-group mb-3">
+                <label htmlFor="email" className="email-cadastro">
+                  Qual o seu e-mail?
+                </label>
+                <input
+                  type="email"
+                  className="form-control"
+                  id="email"
+                  placeholder="Insira seu e-mail."
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  required
+                />
+              </div>
 
-            <div className="form-group mb-3">
-              <label htmlFor="senha" className="senha-cadastro">
-                Informe sua senha.
-              </label>
-              <input
-                type="senha"
-                className="form-control"
-                id="senha"
-                placeholder="Informe sua senha."
-                value={senha}
-                onChange={(e) => setsenha(e.target.value)}
-                required
-              />
-            </div>
+              <div className="form-group mb-3">
+                <label htmlFor="senha" className="senha-cadastro">
+                  Informe sua senha.
+                </label>
+                <input
+                  type="senha"
+                  className="form-control"
+                  id="senha"
+                  placeholder="Informe sua senha."
+                  value={senha}
+                  onChange={(e) => setsenha(e.target.value)}
+                  required
+                />
+              </div>
 
-            <div className="text-center mt-4">
-              <button type="submit" className="btn btn-success">
-                Login
-              </button>
-              {error && (
-                <p
-                  className={
-                    error.includes("não correspondem")
-                      ? "error-message"
-                      : "success-message"
-                  }
-                >
-                  {error}
-                </p>
-              )}
-            </div>
-          </form>
-          <br></br>
-          <br></br>
+              <div className="text-center mt-4">
+                <button type="submit" className="btn btn-success">
+                  Login
+                </button>
+                {error && (
+                  <p
+                    className={
+                      error.includes("não correspondem")
+                        ? "error-message"
+                        : "success-message"
+                    }
+                  >
+                    {error}
+                  </p>
+                )}
+              </div>
+            </form>
+            <br></br>
+            <br></br>
+          </div>
         </div>
       </div>
-    </div></section>
+    </section>
   );
 }
 
